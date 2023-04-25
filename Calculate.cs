e55bc3908af508c406a1e571976531a402e93059
William Garcia <114087502+EarthWarrior621@users.noreply.github.com>
2023-04-25 09:50:27 -0500

    Initial commit

---
+++ Calculate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    static class Calculate
+    {
+
+        public static float result = 0.0f;
+
+        static Calculate()
+        {
+            result = 0.0f;
+        }
+
+        // Integers
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+       
+        // Floating 
+        
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+        public static float Sub(float x, float y)
+        {
+            return x - y;
+        }
+        public static float Mult(float x, float y)
+        {
+            return x * y;
+        }
+        public static float Div(float x, float y)
+        {
+            return x / y;
+        }
+    }
+}
